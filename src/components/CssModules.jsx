39013cf40619ea1db32ec0c8332ab31379ca0799
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-03 22:01:13 +0900

    CSSモジュール（いつもの使い方）

--- src/components/CssModules.jsx
+++ src/components/CssModules.jsx
@@ -1,8 +1,10 @@
+import classes from "./CssModules.module.scss";
+
 export const CssModules = () => {
   return (
-    <div>
-      <p>- CSS Modules -</p>
-      <button></button>
+    <div className={classes.container}>
+      <p className={classes.title}>- CSS Modules -</p>
+      <button className={classes.button}>FIGHT!!</button>
     </div>
   );
 };
