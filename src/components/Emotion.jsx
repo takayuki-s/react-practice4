0425fbc592ab549a0356b39bc12aa83eac1ea1db
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-05 09:56:19 +0900

    scssと同じ書き方でCSS適用

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,6 +1,19 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
 export const Emotion = () => {
+  const containerStyle = css`
+    border: solid 2px #392eff;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
   return (
-    <div>
+    <div css={containerStyle}>
       <p>- Emotion -</p>
       <button>FIGHT!!!!!</button>
     </div>
