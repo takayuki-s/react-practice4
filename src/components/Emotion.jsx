4cd0b228238a031db1d630465c9e13bb4886af9a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-05 10:04:44 +0900

    @emotion.styledライブラリを使ったCSS適用

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,6 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 export const Emotion = () => {
   const containerStyle = css`
@@ -19,7 +20,19 @@ export const Emotion = () => {
   return (
     <div css={containerStyle}>
       <p css={titleStyle}>- Emotion -</p>
-      <button>FIGHT!!!!!</button>
+      <SButton>FIGHT!!!!!</SButton>
     </div>
   );
 };
+
+const SButton = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
